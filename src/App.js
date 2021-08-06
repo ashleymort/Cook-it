3dd49035442b461965a8d2861508fcfac4df3df7
Ashley M <ashleymort@hotmail.co.uk>
2021-08-06 18:14:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cook-itt</h1>
+        <form>
+          <label>SearchRecipes</label>
+          <input type="text" />
+          <input type="submit" />
+        </form>
       </header>
+      <main>
+
+      </main>
+      <aside>
+        <h2>subReditts</h2>
+
+      </aside>
     </div>
   );
 }
